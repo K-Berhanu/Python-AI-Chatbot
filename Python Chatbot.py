b13abf17e981df275d86a1373d6be94f0ab98f99
K-Berhanu <157245921+K-Berhanu@users.noreply.github.com>
2026-06-26 15:14:08 -0400

    Add files via upload

---
+++ Python Chatbot.py
@@ -0,0 +1,44 @@
+'''
+import openai
+
+openai.api_key = "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"
+
+def chat_with_gpt(prompt):
+    response = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",
+            messages = [{"role":"user","content":prompt}]
+        )
+    return response.choices[0].message.content.strip()
+if __name__ == "__main__":
+    while True:
+        user_input = ("You: ")
+        if user_input.lower() in ["quit","exit", "bye"]:
+            break
+
+        response = chat_with_gpt(user_input)
+        print("Chatbot: ", response)
+'''
+from openai import OpenAI
+
+client = OpenAI(api_key="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")
+
+def chat_with_gpt(prompt):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return response.choices[0].message.content.strip()
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("You: ")
+
+        if user_input.lower() in ["quit", "exit", "bye"]:
+            break
+
+        response = chat_with_gpt(user_input)
+        print("Chatbot:", response)
+
+#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        
